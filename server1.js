81021f405ce838a93f8c9c7541960847650d9224
OH-JOO-YEONG <brb1111@naver.com>
2020-05-30 01:20:32 +0900

    Cookie made in server

--- server1.js
+++ server1.js
@@ -1,5 +1,7 @@
 const http = require("http");
-// const fs = require("fs");
+const fs = require("fs");
+const url = require("url");
+const qs = require("querystring");
 
 const parseCookies = (cookie = "") =>
     cookie
@@ -13,10 +15,29 @@ const parseCookies = (cookie = "") =>
 
 const server = http
     .createServer((req, res) => {
-        console.log(req.url, parseCookies(req.headers.cookie));
-        res.writeHead(200, { "Set-Cookie": "mycookie=test" });
-        res.end("Hello Cookie");
-        console.log("서버 실행");
+        const cookies = parseCookies(req.headers.cookies);
+        if (req.url.startsWith("/login")) {
+            const { query } = url.parse(req.url);
+            const { name } = qs.parse(query);
+            const expires = new Date();
+            expires.setMinutes(expires.getMinutes() + 5);
+            // name은 입력한 이름, Expires는 쿠키의 유효시간, cookie의 접근방식은 웹에서만 http 자바스크립트 접근x, 루트경로에서만 유효한
+            res.writeHead(302, {
+                Location: "/",
+                "Set-Cookie": `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            });
+            res.end();
+        } else if (cookies.name) {
+            res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+            res.end(`${cookies.name}님 안녕하세요`);
+        } else {
+            fs.readFile("./server4.html", (err, data) => {
+                res.end(data);
+            });
+        }
+        // res.writeHead(200, { "Set-Cookie": "mycookie=test" });
+        // res.end("Hello Cookie");
+        // console.log("서버 실행");
         // fs.readFile("./server2.html", (err, data) => {
         //     if (err) {
         //         throw err;
