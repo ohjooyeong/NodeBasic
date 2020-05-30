fd0bb4caf3acd1e2e9b4f4f49b8ab4f6f4973e94
OH-JOO-YEONG <brb1111@naver.com>
2020-05-30 16:23:38 +0900

    REST API notion and req.method branch treat

--- server1.js
+++ server1.js
@@ -7,29 +7,36 @@ const parseCookies = (cookie = "") =>
     cookie
         .split(";")
         .map((v) => v.split("="))
-        .map(([k, ...vs]) => [k, vs.join("=")])
         .reduce((acc, [k, v]) => {
             acc[k.trim()] = decodeURIComponent(v);
             return acc;
         }, {});
 
+const session = {};
+
 const server = http
     .createServer((req, res) => {
-        const cookies = parseCookies(req.headers.cookies);
+        const cookies = parseCookies(req.headers.cookie);
         if (req.url.startsWith("/login")) {
             const { query } = url.parse(req.url);
             const { name } = qs.parse(query);
             const expires = new Date();
             expires.setMinutes(expires.getMinutes() + 5);
+            const randomInt = +new Date();
+            session[randomInt] = {
+                name,
+                expires,
+            };
+
             // name은 입력한 이름, Expires는 쿠키의 유효시간, cookie의 접근방식은 웹에서만 http 자바스크립트 접근x, 루트경로에서만 유효한
             res.writeHead(302, {
                 Location: "/",
-                "Set-Cookie": `name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+                "Set-Cookie": `session=${randomInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
             });
             res.end();
-        } else if (cookies.name) {
+        } else if (cookies.session && session[cookies.session].expires > new Date()) {
             res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-            res.end(`${cookies.name}님 안녕하세요`);
+            res.end(`${session[cookies.session].name}님 안녕하세요`);
         } else {
             fs.readFile("./server4.html", (err, data) => {
                 res.end(data);
